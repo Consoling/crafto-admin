46b4098f54bf4e215ef5f210722307b78dbdee30
Consoling <shahidarizfannymagnet@gmail.com>
2025-03-23 13:55:17 +0530

    cron test

--- app/api/cron-deploy/route.ts
+++ app/api/cron-deploy/route.ts
@@ -24,7 +24,7 @@ export async function POST(req: NextRequest, ) {
   
     await execPromise('git pull origin master');
 
-// Build the Next.js app
+
     await execPromise('npm run build');
 
     
